287388f8a6010e960094529c9927b9dfac18ca64
Brian <brian.lee@servicemax.com>
2016-09-27 14:43:33 -0700

    refactored some

--- my-workspace/theme-demo-app/gulpfile.js
+++ my-workspace/theme-demo-app/gulpfile.js
@@ -4,7 +4,7 @@ var shell = require('gulp-shell')
 
 gulp.task('copy-css', function() {
     return gulp.src('../build/production/ThemeDemoApp/resources/*.css')
-        .pipe(gulp.dest('/Users/brianlee/Documents/Cornerstone/uifw.trunk/testapps/csskitchensinkapp'));
+        .pipe(gulp.dest('/Users/brianlee/Documents/Cornerstone/uifw.trunk/testapps/csskitchensinkapp/modules/com.servicemax.client.uikitchensinkmodern/resources/css'));
 })
 
 gulp.task('default', ['copy-css']);
